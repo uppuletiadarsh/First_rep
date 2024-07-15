3f99a55c86e8ab0e312780675d6bdcbc6ffc75d2
Adarsh <adarshuppuleti@gmail.com>
2024-07-15 12:25:45 +0530

    third_Commit

---
+++ Transfer.py
@@ -0,0 +1,20 @@
+#  break statement
+for i in range(5):
+    if i == 3:
+        break
+    print(i)
+
+#  continue statement
+for i in range(5):
+    if i == 3:
+        continue
+    print(i)
+
+# pass statement
+for i in range(5):
+    if i == 3:
+        pass
+    else:
+        print(i)
+
+
